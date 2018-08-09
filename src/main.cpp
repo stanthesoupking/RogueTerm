d76368c024a7572a48b52f1feaed2eaed1d8a54b
Stanley Fuller <stanthesoupking@gmail.com>
2018-08-09 16:28:02 +1000

    Fixed memory leaks + all new and improved demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,23 @@
 #include "RogueTerm/RogueTerm.h"
 
+#define WINDOW_COLUMNS  64
+#define WINDOW_ROWS     32
+
+// The font to use in the game window
+RT_Font *font = NULL;
+
 // The main game window
-RT_Window* window = NULL;
+RT_Window *window = NULL;
 
 // Method declarations
 bool init();
 void cleanup();
 void startLoop();
 void render();
+
+int player_x, player_y;
+RT_Colour player_colour;
+char player_char;
 
 int main(int argc, char **argv) {
     // Init RogueTerm window, quit if init fails
@@ -23,16 +33,24 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-// Initialise the RogueTerm window
+// Initialise the demo
 bool init() {
-    RT_Font* font = new RT_Font("./fonts/dejavu.png");
-    window = new RT_Window(font, 32, 24, "RogueTerm - Demo");
+    font = new RT_Font("./fonts/terminal.png");
+    window = new RT_Window(font, WINDOW_COLUMNS, WINDOW_ROWS, "RogueTerm - Demo");
+
+    // Initialise player in the center of the 'room'
+    player_x = WINDOW_COLUMNS / 2;
+    player_y = WINDOW_ROWS / 2;
+    player_colour = {255,255,0};
+    player_char = 'C';
+
     return true;
 }
 
 // Clean up variables
 void cleanup() {
-    // TODO
+    delete window;
+    delete font;
 }
 
 // Start main program loop
@@ -50,7 +68,25 @@ void startLoop() {
             if(e.type == SDL_QUIT) {
                 quit = true;
             }
+            else if (e.type == SDL_KEYDOWN) {
+                switch(e.key.keysym.sym) {
+                    case SDLK_UP:
+                        player_y--;
+                        break;
+                    case SDLK_DOWN:
+                        player_y++;
+                        break;
+                    case SDLK_LEFT:
+                        player_x--;
+                        break;
+                    case SDLK_RIGHT:
+                        player_x++;
+                        break;
+                }
+            }
         }
+
+        printf("X:%d, Y%d\n", player_x, player_y);
         
         render(); // Render scene
     }
@@ -60,9 +96,13 @@ void startLoop() {
 void render() {
     window->clear();
 
-    RT_Colour colour = {0, 255, 0};
+    RT_Colour title_colour = {255,255,255};
+    RT_Colour subtitle_colour = {200,200,200};
 
-    window->draw_string("Hello, world.",1,1,&colour);
+    window->draw_string("Welcome to the RogueTerm demo!",1,1, &title_colour);
+    window->draw_string("Use the arrow keys to move the character around...", 1, 2, &subtitle_colour);
+
+    window->draw_char(player_char, player_x, player_y, &player_colour);
 
     window->update();
 }
