a2545e0e67ee184404bb33bd6c7c48609f593381
Stanley Fuller <stanthesoupking@gmail.com>
2018-08-09 17:08:43 +1000

    Improve character mapping to font file

--- src/RogueTerm/RT_Font.cpp
+++ src/RogueTerm/RT_Font.cpp
@@ -39,13 +39,29 @@ SDL_Rect RT_Font::get_char_sdl_rect(char c) {
         row = 0;
         col = c - 32;
     }
+    else if(c == 64) {
+        row = 1;
+        col = 0;
+    }
     else if((65 <= c) && (c <= 90)) {
         row = 3;
         col = c - 65;
     }
-    else if(97 < c < 122) {
+    else if((97 <= c) && (c <= 122)) {
         row = 4;
         col = c - 97;
+    }
+    else if((91 <= c) && (c <= 96)) {
+        row = 1;
+        col = c - 90;
+    }
+    else if((123 <= c) && (c <= 126)) {
+        row = 1;
+        col = c - 116;
+    }
+    else if((176 <= c) && (c <= 181)) {
+        row = 1;
+        col = c - 165;
     }
 
     return {col*character_dimensions.width,
@@ -55,19 +71,24 @@ SDL_Rect RT_Font::get_char_sdl_rect(char c) {
 }
 
 void RT_Font::blit_char(char c, SDL_Surface* dst, int x, int y, RT_Colour *colour) {
+    // Generate the rect of character texture and output rect
     SDL_Rect crect = get_char_sdl_rect(c);
     SDL_Rect drect = {x, y, crect.w, crect.h};
 
+    // Blit character to the destination surface
     SDL_BlitSurface(surface, &crect, dst, &drect);
 
     // If a colour was given then apply it
     if(colour != NULL) {
+        // Generate surface filled with the given colour
         SDL_Surface *cmod = SDL_CreateRGBSurface(0, character_dimensions.width, character_dimensions.height, 32, 0, 0, 0, 0);
         SDL_FillRect(cmod, NULL, SDL_MapRGB(cmod->format, colour->r, colour->g, colour->b));
 
+        // Blit colour modulator surface to the destination surface
         SDL_SetSurfaceBlendMode(cmod, SDL_BLENDMODE_MOD);
         SDL_BlitSurface(cmod, NULL, dst, &drect);
 
+        // Free cmod from memory
         SDL_FreeSurface(cmod);
     }
 }
